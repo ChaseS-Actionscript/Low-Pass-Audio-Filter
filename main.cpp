b417b86112f3faaa1257384e9af8c01d5e79d0ba
Mocha-Blue <chasesaltzman07@gmail.com>
2025-10-12 02:16:36 -0400

    added optimization with memcpy for header and a print function to see the header attributes

--- main.cpp
+++ main.cpp
@@ -4,11 +4,7 @@
 #include <chrono>
 #include <thread>
 #include <filesystem>
-int MEM_fread(unsigned char* buf, size_t size, size_t n, unsigned char* *f) {
-    memcpy(buf, *f, size * n);
-    *f += size * n;
-    return n;
-}
+
 typedef struct WAV_HEADER{
     char                RIFF[4];        // RIFF Header      Magic header
     uint32_t       ChunkSize;      // RIFF Chunk Size  
@@ -23,17 +19,59 @@ typedef struct WAV_HEADER{
     unsigned short      bitsPerSample;  // Number of bits per sample      
     char                Subchunk2ID[4]; // "data"  string   
     uint32_t       Subchunk2Size;  // Sampled data length    
-}wav_hdr; 
+}wav_hdr;
+
+void printAttri(wav_hdr wavHeader){
+    using namespace std;
+     cout << "RIFF header                :" << wavHeader.RIFF[0] 
+                                                << wavHeader.RIFF[1] 
+                                                << wavHeader.RIFF[2] 
+                                                << wavHeader.RIFF[3] << endl;
+
+        cout << "WAVE header                :" << wavHeader.WAVE[0] 
+                                                << wavHeader.WAVE[1] 
+                                                << wavHeader.WAVE[2] 
+                                                << wavHeader.WAVE[3] 
+                                                << endl;
+
+        cout << "FMT                        :" << wavHeader.fmt[0] 
+                                                << wavHeader.fmt[1] 
+                                                << wavHeader.fmt[2] 
+                                                << wavHeader.fmt[3] 
+                                                << endl;
+
+        cout << "Data size                  :" << wavHeader.ChunkSize << endl;
+
+        // Display the sampling Rate form the header
+        cout << "Sampling Rate              :" << wavHeader.SamplesPerSec << endl;
+        cout << "Number of bits used        :" << wavHeader.bitsPerSample << endl;
+        cout << "Number of channels         :" << wavHeader.NumOfChan << endl;
+        cout << "Number of bytes per second :" << wavHeader.bytesPerSec << endl;
+        cout << "Data length                :" << wavHeader.Subchunk2Size << endl;
+        cout << "Audio Format               :" << wavHeader.AudioFormat << endl;
+        // Audio format 1=PCM,6=mulaw,7=alaw, 257=IBM Mu-Law, 258=IBM A-Law, 259=ADPCM 
+
+        cout << "Block align                :" << wavHeader.blockAlign << endl;
+
+        cout << "Data string                :" << wavHeader.Subchunk2ID[0] 
+                                                << wavHeader.Subchunk2ID[1]
+                                                << wavHeader.Subchunk2ID[2] 
+                                                << wavHeader.Subchunk2ID[3] 
+                                                << endl;
+}
+
 uint16_t bytesToShort(char first, char second){
     return (uint16_t)((unsigned char)first | (unsigned char)second << 8);
 }
+
 uint32_t bytesToInt32(char first, char second, char third, char fourth){
     return (uint32_t)((unsigned char)first | 
         (unsigned char)second << 8 |
         (unsigned char)third << 16 | 
         (unsigned char)fourth << 24); 
 }
-void pointerArithTest(char* array, size_t size){
+
+void readWaveData(char* array, size_t size){
     uint32_t temp = (uint32_t)(array); 
     temp += sizeof(char)*(size-1);
     for(; array<(char*)temp; array+=2){
@@ -56,6 +94,7 @@ void writeBackToFile(char* header, char* buffer, size_t sizeOfBuffer){
     ofs.write(buffer, sizeOfBuffer);
     
 }
+
 void writeBackToFileCsv(unsigned char* buffer, size_t sizeOfBuffer, std::string fileName){
     std::string filePath = (std::filesystem::current_path() / fileName).string();
     std::fstream ofs(filePath.c_str(), std::ios::out | std::ios::binary);
@@ -66,6 +105,7 @@ void writeBackToFileCsv(unsigned char* buffer, size_t sizeOfBuffer, std::string 
         ofs << ((unsigned short)(buffer[i]) | (unsigned short)(buffer[i+1]) << 8) << ", ";
     }
 }
+
 int main(){
 
     std::string filePath = (std::filesystem::current_path() / "file.wav").string();
@@ -87,22 +127,14 @@ int main(){
 
 
     wav_hdr wavHeader;
-    wavHeader.RIFF[0] = header[0];
-    wavHeader.RIFF[1] = header[1];
-    wavHeader.RIFF[2] = header[2];
-    wavHeader.RIFF[3] = header[3];
 
+    memcpy(wavHeader.RIFF, &header[0], 4);
+    
     wavHeader.ChunkSize = bytesToInt32(header[4], header[5], header[6], header[7]);
     
-    wavHeader.WAVE[0] = header[8];
-    wavHeader.WAVE[1] = header[9];
-    wavHeader.WAVE[2] = header[10];
-    wavHeader.WAVE[3] = header[11];
+    memcpy(wavHeader.WAVE, &header[8], 4);
 
-    wavHeader.fmt[0] = header[12];
-    wavHeader.fmt[1] = header[13];
-    wavHeader.fmt[2] = header[14];
-    wavHeader.fmt[3] = header[15];
+    memcpy(wavHeader.fmt, &header[12], 4);
 
     wavHeader.Subchunk1Size = bytesToInt32(header[16], header[17], header[18], header[19]);
 
@@ -118,21 +150,16 @@ int main(){
 
     wavHeader.bitsPerSample = bytesToShort(header[34], header[35]);
 
-    wavHeader.Subchunk2ID[0] = header[36];
-    wavHeader.Subchunk2ID[1] = header[37];
-    wavHeader.Subchunk2ID[2] = header[38];
-    wavHeader.Subchunk2ID[3] = header[39];
+    memcpy(wavHeader.Subchunk2ID, &header[36], 4);
 
     wavHeader.Subchunk2Size = bytesToInt32(header[40], header[41], header[42], header[43]);
     
     writeBackToFileCsv(reinterpret_cast<unsigned char*>(buffer), length, "out1.csv");
-    pointerArithTest(buffer, length);
+    readWaveData(buffer, length);
     writeBackToFile(header, buffer, length);
     writeBackToFileCsv(reinterpret_cast<unsigned char*>(buffer), length, "out2.csv");
-    
-    std::cout << std::endl;
-    std::cout << std::endl;
-    std::cout << wavHeader.Subchunk2Size;
+    printAttri(wavHeader);
+
     delete buffer;
     delete header;
     return 0;
